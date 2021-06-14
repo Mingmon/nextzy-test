f3967bc34fd55499ca46fc77521cc66c383ceaa4
Mingmon <mingmon123@hotmail.com>
2021-06-14 22:18:11 +0700

    Added Binding And Test Show News Article
    Added Binding And Test Show Text

--- app/src/main/java/com/example/android/newsapplication/newsList/NewsListFragment.kt
+++ app/src/main/java/com/example/android/newsapplication/newsList/NewsListFragment.kt
@@ -1,32 +1,32 @@
 package com.example.android.newsapplication.newsList
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.example.android.newsapplication.R
+import com.example.android.newsapplication.databinding.NewsListFragmentBinding
+import com.example.android.newsapplication.model.NewsResponse
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class NewsListFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = NewsListFragment()
-    }
+    val newsListViewModel: NewsListViewModel by viewModel()
+    private lateinit var newsListBinding: NewsListFragmentBinding
 
-    private lateinit var viewModel: NewsListViewModel
+    private val observer = Observer<NewsResponse>{
+        newsListBinding.testTextView.text = "test get title from article news index 0 = ${it.articles?.get(0)?.title}"
+    }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.news_list_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(NewsListViewModel::class.java)
-        // TODO: Use the ViewModel
+        newsListBinding = DataBindingUtil.inflate(inflater,R.layout.news_list_fragment, container, false)
+        newsListViewModel.news.observe(viewLifecycleOwner,observer)
+        return newsListBinding.root
     }
 
 }
