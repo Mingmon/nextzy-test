05d2e0cac28281438c6df46fe32623d8df15f8a3
Mingmon <mingmon123@hotmail.com>
2021-06-14 22:09:49 +0700

    Added Repository
    Added Repository

---
+++ app/src/main/java/com/example/android/newsapplication/repo/NewsRepository.kt
@@ -0,0 +1,10 @@
+package com.example.android.newsapplication.repo
+
+import com.example.android.newsapplication.datasource.NewsApi
+
+class NewsRepository(private val newsApi: NewsApi){
+    suspend fun getNews() = newsApi.getNews()
+}
+
+
+
