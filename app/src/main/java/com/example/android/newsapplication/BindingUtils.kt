1f8342986bea7d7765e74b067988c53592b2ee81
Mingmon <mingmon123@hotmail.com>
2021-06-15 21:00:51 +0700

    Added Binding News Detail
    Added Binding News Detail

--- app/src/main/java/com/example/android/newsapplication/BindingUtils.kt
+++ app/src/main/java/com/example/android/newsapplication/BindingUtils.kt
@@ -1,10 +1,12 @@
 package com.example.android.newsapplication
 
+import android.annotation.SuppressLint
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.MultiTransformation
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.FitCenter
 import com.bumptech.glide.load.resource.bitmap.GranularRoundedCorners
 import com.example.android.newsapplication.model.NewsDetail
@@ -23,15 +25,58 @@ fun TextView.sourceText(news: NewsDetail?) {
     }
 }
 
+@BindingAdapter("authorText")
+fun TextView.setAuthorText(news: NewsDetail?) {
+    news?.author?.let {
+        text = it.substringAfterLast("/").replace("-"," ")
+    }
+}
+@BindingAdapter("dateTimeText")
+fun TextView.setDateTimeText(news: NewsDetail?) {
+    news?.publishedAt?.let {
+        text = it.replace("[-TZ]".toRegex()," ")
+    }
+}
+
+@BindingAdapter("contentText")
+fun TextView.setContentText(news: NewsDetail?) {
+    news?.content?.let {
+        text = it
+    }
+}
+
+@SuppressLint("SetTextI18n")
+@BindingAdapter("urlText")
+fun TextView.setUrlText(news: NewsDetail?) {
+    news?.url?.let {
+        text = "Source : $it"
+    }
+}
+
 @BindingAdapter("newsImage")
-fun ImageView.setNewsImage(news: NewsDetail?){
+fun ImageView.setNewsImage(news: NewsDetail?) {
+    news?.urlToImage?.let {
+        Glide.with(this)
+            .load(it)
+            .transform(
+                MultiTransformation(
+                    CenterCrop(),
+                    GranularRoundedCorners(15.0F, 0F, 0F, 15.0F)
+                )
+            )
+            .into(this)
+    }
+}
+
+@BindingAdapter("newsDetailImage")
+fun ImageView.setNewsDetailImage(news: NewsDetail?) {
     news?.urlToImage?.let {
         Glide.with(this)
             .load(it)
             .transform(
                 MultiTransformation(
                     FitCenter(),
-                    GranularRoundedCorners(15.0F, 0F, 0F, 15.0F)
+                    GranularRoundedCorners(0.0F, 0F, 100.0F, 100.0F)
                 )
             )
             .into(this)
