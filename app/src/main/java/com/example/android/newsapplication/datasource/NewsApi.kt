bb73361e86897e33956f61cfe85a19f08aaf3c5d
Mingmon <mingmon123@hotmail.com>
2021-06-15 20:59:21 +0700

    Change News Category Api
    Change News Category Api

--- app/src/main/java/com/example/android/newsapplication/datasource/NewsApi.kt
+++ app/src/main/java/com/example/android/newsapplication/datasource/NewsApi.kt
@@ -4,6 +4,6 @@ import com.example.android.newsapplication.model.NewsResponse
 import retrofit2.http.GET
 
 interface NewsApi {
-    @GET("everything?q=Apple")
+    @GET("everything?q=Android")
     suspend fun getNews(): NewsResponse
 }
